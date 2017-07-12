9a5e573d82e7dc867af598d34d538ce851054f60
zhangbobell <zhangbobell@163.com>
2017-07-12 18:00:44 +0800

    test(renders): add X axis test case in flip and rotate render

--- tests/renders/rotate.spec.ts
+++ tests/renders/rotate.spec.ts
@@ -16,18 +16,33 @@ describe('test rotate render', () => {
 
     beforeEach(() => {
         slide = new Rotate();
+    });
+
+    test('normal rotate render in Y axis', () => {
         mockSwiper = {
             axis: 'Y',
             sideOffset: -65,
             sideLength: 650
         };
-    });
 
-    test('normal rotate render', () => {
         let transform = slide.doRender(mockSwiper);
         
         expect(transform.swiper).toBe('-webkit-perspective:2600px;-webkit-transform-style:preserve-3d;');
         expect(transform.currentPage).toBe('-webkit-transform: rotateX(9deg) translateZ(288.925px) scale(0.889);');
         expect(transform.activePage).toBe('-webkit-transform: rotateX(-81deg) translateZ(288.925px) scale(0.889);');
+    });
+
+    test('normal rotate render in X axis', () => {
+        mockSwiper = {
+            axis: 'X',
+            sideOffset: -40,
+            sideLength: 400
+        };
+
+        let transform = slide.doRender(mockSwiper);
+        
+        expect(transform.swiper).toBe('-webkit-perspective:1600px;-webkit-transform-style:preserve-3d;');
+        expect(transform.currentPage).toBe('-webkit-transform: rotateY(-9deg) translateZ(177.8px) scale(0.889);');
+        expect(transform.activePage).toBe('-webkit-transform: rotateY(81deg) translateZ(177.8px) scale(0.889);');
     });
 });
