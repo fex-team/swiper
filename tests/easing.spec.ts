597728ad03184898202b3b9259400de8a29dd201
zhangbobell <zhangbobell@163.com>
2017-07-12 13:45:57 +0800

    test(easing): add native number test to easing functions

--- tests/easing.spec.ts
+++ tests/easing.spec.ts
@@ -14,12 +14,14 @@ describe('testing easing class', () => {
     test('test easeOutQuad', () => {
         expect(Easing.easeOutQuad(0, 650)).toBe(0);
         expect(Easing.easeOutQuad(325, 650)).toBe(406.25);
+        expect(Easing.easeOutQuad(-325, 650)).toBe(-406.25);
         expect(Easing.easeOutQuad(650, 650)).toBe(650);      
     });
 
     test('test rubberBand', () => {
         expect(Easing.rubberBand(0, 650)).toBe(0);
         expect(Easing.rubberBand(325, 650)).toBeCloseTo(140.2);
+        expect(Easing.rubberBand(-325, 650)).toBeCloseTo(-140.2);
         expect(Easing.rubberBand(650, 650)).toBeCloseTo(230.65);
     });
 
